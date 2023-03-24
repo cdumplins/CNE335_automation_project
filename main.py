f4404b026ab5e6da0615d39b748c19a120149506
cdumplins <117939561+cdumplins@users.noreply.github.com>
2023-03-24 00:05:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-# This is the template code for the CNE335 Final Project
-# Justin Ellis
-# CNE 335 Fall
+import os
+
+class Server:
+    """ Server class for representing and manipulating servers. """
+
+    def __init__(self, server_ip):
+        self.server_ip = server_ip
+
+    def ping(self):
+        response = os.system("ping -c 1 " + self.server_ip)
+        if response == 0:
+            return "Server is up!"
+        else:
+            return "Server is down!"
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Calvin")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+    server = Server("35.86.106.84")
+    result = server.ping()
+    print(result)
